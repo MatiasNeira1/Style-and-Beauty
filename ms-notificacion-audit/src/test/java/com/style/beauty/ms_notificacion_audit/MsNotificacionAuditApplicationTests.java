c1744e888fdb3384edb5f8b41f807683281a0e41
Matias Salas <mati.neiras@duocuc.cl>
2026-04-29 20:12:51 -0400

    Add files via upload

---
+++ ms-notificacion-audit/src/test/java/com/style/beauty/ms_notificacion_audit/MsNotificacionAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.style.beauty.ms_notificacion_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificacionAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
