b1afbf270fbe75f5416336dd85880bd981018e0e
Matias Salas <mati.neiras@duocuc.cl>
2026-04-29 20:15:47 -0400

    Add files via upload

---
+++ ms-extra/src/main/java/com/style/beauty/ms_extra/MsExtraApplication.java
@@ -0,0 +1,13 @@
+package com.style.beauty.ms_extra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsExtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsExtraApplication.class, args);
+	}
+
+}
---
+++ ms-extra/src/test/java/com/style/beauty/ms_extra/MsExtraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.style.beauty.ms_extra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsExtraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
