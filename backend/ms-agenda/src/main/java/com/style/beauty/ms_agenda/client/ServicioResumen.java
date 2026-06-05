85b29e7148345457f9e6c10f8c014bd2aed04660
matias <matiasjasban98@gmail.com>
2026-06-04 22:36:44 -0400

    Correción de ms-agenda

--- backend/ms-agenda/src/main/java/com/style/beauty/ms_agenda/client/ServicioResumen.java
+++ backend/ms-agenda/src/main/java/com/style/beauty/ms_agenda/client/ServicioResumen.java
@@ -5,12 +5,17 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import java.util.UUID;
 
 public record ServicioResumen(
-        @JsonAlias({"id_servicio", "idServicio", "id"})
+       @JsonAlias({"id_servicio", "idServicio", "id"})
         UUID idServicio,
+
         String nombre,
+
         String categoria,
+
         @JsonAlias({"duracion_minutos", "duracionMinutos", "duracionServicioMin", "duracion"})
         Integer duracionMinutos,
+
         @JsonAlias({"holgura_minutos", "holguraMinutos", "holguraMin"})
-        Integer holguraMinutos) {
+        Integer holguraMinutos
+){
 }
